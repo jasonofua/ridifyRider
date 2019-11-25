1f66daeb292ed90fef2b6f787d3cc4149b5988d2
jasonofua <jasonofua@gmail.com>
2019-11-25 20:26:52 +0100

    cleaned it

--- ridify/ios/Runner/AppDelegate.m
+++ ridify/ios/Runner/AppDelegate.m
@@ -1,12 +1,19 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
+// Add the GoogleMaps import.
+#import "GoogleMaps/GoogleMaps.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+
+   // Add the following line with your API key.
+    [GMSServices provideAPIKey:@"AIzaSyDQfIpM2KtOjLegQ65vxJPeHGNtIVY4qCA"];
+    [GeneratedPluginRegistrant registerWithRegistry:self];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
